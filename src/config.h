46e5075b4ae4ef77e28a27a1c7b789ab09cb0c96
Cory Logan <he@corylogan.com>
2020-12-08 13:23:06 -0700

    adding sides

--- src/config.h
+++ src/config.h
@@ -3,7 +3,7 @@
 
 #define ANIM_DEBUG false
 
-const int NUMPIXELS = 70;
+const int NUMPIXELS = 100;
 
 // Here we define boards as integers to be used in `platform.ini` passed build
 // flags. This way we can use the easier to reference varaiable name to keep the
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,40 @@
-#include "config.h"
 #include <Arduino.h>
+
+#include "config.h"
 #define FASTLED_INTERNAL
+#include <SPI.h>
+
 #include "DebugLog.h"
 #include "FastLED.h"
 #include "MilliTimer.h"
-#include <SPI.h>
- 
+
 MilliTimer timer{};
 
 CRGB leds[NUMPIXELS];
 
+int legLength = NUMPIXELS / 5;
+int sideLength = legLength / 2;
+int numSides = 10;
 
 uint8_t hue = 0;
 void setup() {
   FastLED.addLeds<WS2811, DATAPIN, GRB>(leds, NUMPIXELS);
   Serial.begin(115200);
-
 }
 
 void loop() {
   if (timer.hasElapsedWithReset(100)) {
-    fill_solid(leds, NUMPIXELS, CHSV(hue++, 255, 255));
+    // fill_solid(leds, NUMPIXELS, CHSV(hue++, 255, 255));
+    for (int i = 0; i < numSides; i++) {
+      for (int ii = 0; ii < sideLength; ii++) {
+        int offset = (i * sideLength) + ii;
+        if (i % 2 == 0) {
+          leds[offset].setRGB(0, 255, 0);
+        } else {
+          leds[offset].setRGB(255,0,  0);
+        }
+      }
+    }
     FastLED.show();
   }
 }
